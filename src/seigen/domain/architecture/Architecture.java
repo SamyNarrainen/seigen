87a5c0a87d9edafc4c00c93862030a428842176c
Samy Narrainen <samynarrainen@gmail.com>
2019-06-23 21:39:52 +0100

    Added JavaDoc to interface.

--- src/seigen/domain/architecture/Architecture.java
+++ src/seigen/domain/architecture/Architecture.java
@@ -4,6 +4,9 @@ import seigen.domain.server.ApplicationServer;
 
 import java.util.List;
 
+/**
+ * Interface which provides an API to accessing the servers associated with a distributed server architecture.
+ */
 public interface Architecture {
 
     List<ApplicationServer> getApplicationServerList();
